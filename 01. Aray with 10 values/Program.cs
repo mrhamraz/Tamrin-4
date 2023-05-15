6f5f1439a70d21ed6323b68887ba07d98d12018a
Hamraz <x6.reza@gmail.com>
2023-05-15 23:46:01 +0330

    Add project files.

---
+++ 01. Aray with 10 values/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Aray_with_10_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aray = new int[10];   //Create an array of four elements
+
+            Console.WriteLine("Enter 10 numbers of Aray: ");
+
+            for (int i = 0; i < aray.Length; i++)   //get aray values from user and asign them
+                aray[i] = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int i in aray)  //print aray values
+                Console.WriteLine(i);
+        }
+    }
+}
