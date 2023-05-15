c6e236cc76a4a4d349cf0a06d86fad6da961488d
Hamraz <x6.reza@gmail.com>
2023-05-16 01:42:49 +0330

    4.4

---
+++ 04. Sum of values of an array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4._Sum_of_values_of_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of the array: ");  //Reads size of the array
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[size];   //Create an array of four elements
+
+            Console.WriteLine($"Enter {size} members of Array: ");
+
+
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)   //get aray values from user and asign them
+            { 
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                sum += array[i];
+            }
+
+            Console.WriteLine($"sum is {sum}");
+            
+        }
+    }
+}
