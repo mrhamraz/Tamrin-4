a34e3f7d40381ca61111cae89086947f36883aca
Hamraz <x6.reza@gmail.com>
2023-05-16 14:22:00 +0330

    4.3

--- 03. Print aray values reverse/Program.cs
+++ 03. Print aray values reverse/Program.cs
@@ -6,16 +6,16 @@ namespace _3._Print_aray_values_reverse
     {
         static void Main(string[] args)
         {
-            int[] aray = new int[10];   //Create an array
+            int[] array = new int[10];   //Create an array
 
             Console.WriteLine("Enter 10 numbers of Aray: ");
 
-            for (int i = 0; i < aray.Length; i++)   //get aray values from user and asign them
-                aray[i] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)   //get aray values from user and asign them
+                array[i] = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("\nArray in reverse order: ");  //Print array in reversed order
-            for (int i = aray.Length - 1 ; i >= 0; i--)
-                Console.WriteLine(aray[i]);
+            for (int i = array.Length - 1 ; i >= 0; i--)
+                Console.WriteLine(array[i]);
         }
     }
 }
