4c58eca024a9840f7b49a84c48e6a01789bf0966
Hamraz <x6.reza@gmail.com>
2023-05-16 14:31:49 +0330

    4.7

---
+++ 07. Average of array values/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Average_of_array_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of the array: ");  //Reads size of the array
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            double[] array = new double[size];   //Create an array
+
+            Console.WriteLine($"Enter {size} members of Array: ");
+
+            for (int i = 0; i < array.Length; i++)   //get aray values from user and asign them
+                array[i] = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Average is {array.Sum()/size}");    //average of values
+        }
+    }
+}
