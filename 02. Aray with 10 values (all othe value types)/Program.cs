8da5bae197f74130836ce6dd4a9d03745107a6ff
Hamraz <x6.reza@gmail.com>
2023-05-16 14:24:33 +0330

    4.2

--- 02. Aray with 10 values (all othe value types)/Program.cs
+++ 02. Aray with 10 values (all othe value types)/Program.cs
@@ -6,21 +6,21 @@ namespace _02._Aray_with_10_values__all_othe_value_types_
     {
         static void Main(string[] args)
         {
-            string[] aray = new string[10];   //Create an array
-            //bool [] aray = new bool[10];
+            string[] array = new string[10];   //Create an array
+            //bool [] array = new bool[10];
             //char[] array = new char[10];
             //double[] array = new double[10];
 
 
-            Console.WriteLine("Enter 10 strings of Aray: ");
+            Console.WriteLine("Enter 10 strings of array: ");
 
-            for (int i = 0; i < aray.Length; i++)   //get aray values from user and asign them
-                aray[i] = Console.ReadLine();
-                //aray[i] = Convert.ToBoolean(Console.ReadLine());
-                //aray[i] = Convert.ToChar(Console.ReadLine());
-                //aray[i] = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)   //get array values from user and asign them
+                array[i] = Console.ReadLine();
+                //array[i] = Convert.ToBoolean(Console.ReadLine());
+                //array[i] = Convert.ToChar(Console.ReadLine());
+                //array[i] = Convert.ToDouble(Console.ReadLine());
 
-            foreach (string /* bool/char/double */ i in aray)  //print aray values 
+            foreach (string /* bool/char/double */ i in array)  //print array values 
                 Console.WriteLine(i);
         }
     }
