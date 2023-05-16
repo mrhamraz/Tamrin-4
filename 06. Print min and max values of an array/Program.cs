327815eacc0514eeaf6da60f6b318914e4a9ccf1
Hamraz <x6.reza@gmail.com>
2023-05-16 14:18:04 +0330

    4.6

---
+++ 06. Print min and max values of an array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Print_min_and_max_values_of_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of the array: ");  //Reads size of the array
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[size];   //Create an array
+
+            Console.WriteLine($"Enter {size} members of Array: ");
+
+            for (int i = 0; i < array.Length; i++)   //get aray values from user and asign them
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(array.Max());
+            Console.WriteLine(array.Min());
+         
+        }
+    }
+}
