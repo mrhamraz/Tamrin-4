563d5fa7b1dd954c5cd9faf1146f89c1068eae1b
Hamraz <x6.reza@gmail.com>
2023-05-17 23:44:45 +0330

    4.11

---
+++ 11. Compare values of two arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _11._Compare_values_of_two_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[5];   //Create an array
+
+            Console.WriteLine("Enter 5 members of first Array: ");
+
+            for (int i = 0; i < array1.Length; i++)               //get aray values from user and asign them
+                array1[i] = Console.ReadLine();
+
+            string[] array2 = new string[5];   //Create an array
+            Console.WriteLine("Enter 5 members of second Array: ");
+
+            for (int i = 0; i < array2.Length; i++)               //get aray values from user and asign them
+                array2[i] = Console.ReadLine();
+
+
+            for (int i = 0; i < 5; i++)
+                if (array1[i] != array2[i])                       //Compare values
+                    Console.WriteLine(i);
+
+           
+        }
+    }
+}
