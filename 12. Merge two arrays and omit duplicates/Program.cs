ff135d757cd7d3a51ac1e44bf61898e59a7788c5
Hamraz <x6.reza@gmail.com>
2023-05-26 00:26:54 +0330

    4.12

--- 12. Merge two arrays and omit duplicates/Program.cs
+++ 12. Merge two arrays and omit duplicates/Program.cs
@@ -6,59 +6,52 @@ namespace _12._Merge_two_arrays_and_omit_duplicates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter size of the array 1 and 2: ");             //Reads size of the array
+            Console.WriteLine("Enter size of the array 1 and 2: ");              //reads size of the array
             int size1 = Convert.ToInt32(Console.ReadLine());
             int size2 = Convert.ToInt32(Console.ReadLine());
 
-            int[] array1 = new int[size1];                                      //Create an array
-            int[] array2 = new int[size2];                                      //Create an array
+            int[] array1 = new int[size1];                                       //Create an array
+            int[] array2 = new int[size2];                                       //Create an array
 
             Console.WriteLine($"Enter {size1} members of Array 1: ");
-            for (int i = 0; i < array1.Length; i++)                             //get aray values from user and asign them
+            for (int i = 0; i < size1; i++)                                      //get aray values from user and asign them
                 array1[i] = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"Enter {size2} members of Array 2: ");
-            for (int i = 0; i < array2.Length; i++)                             //get aray values from user and asign them
+            for (int i = 0; i < size2; i++)                                      //get aray values from user and asign them
                 array2[i] = Convert.ToInt32(Console.ReadLine());
 
             int newsize = size1 + size2;
             int[] newarray = new int[newsize];
 
-            for (int i = 0; i < array1.Length; i++)
+            for (int i = 0; i < size1; i++)
                 newarray[i] = array1[i];
 
-            for (int i = 0; i < array2.Length; i++)
-                newarray[array1.Length + i] = array2[i];
-
-            int[] newarray2 = new int[newsize];
+            for (int i = 0; i < size2; i++)
+                newarray[size1 + i] = array2[i];
 
 
-            for (int i = 0; i < newsize; i++)                           //finding all duplicate values in array
+            for (int i = 0; i < newsize ; i++)                                 //finding all duplicate values in array
             {
                 for (int j = i + 1; j < newsize; j++)
                 {
-                    if (newarray[i] == newarray[j])                             //if any duplicate found
+                    if (newarray[i] == newarray[j])                              //if any Unique found
                     {
-                        /*
-                        for (int k = j; k < newsize; k++)               //delete the current duplicate value
-                        {
-                            newarray[k] = newarray[k + 1];
+                        for (int k = j; k < newsize-1; k++)                       
+                            newarray[k] = newarray[k + 1];                       //delete the duplicate element and shift following ellemts to left
 
+                        newsize--;                                               //decrement size after removing duplicate element
 
-                            (newsize)--;                                            //Decrement size after removing duplicate element
-
-                            j--;                                                    // If shifting of elements occur then don't increment j
-                        }*/
-                        continue;
+                        j--;                                                     //if shifting of elements occur then don't increase j
                     }
 
-                    newarray2[i] = newarray[i];
                 }
+
             }
 
-            Console.WriteLine("New array elements (duplicates omitted):");
-            foreach (int i in newarray2)
-                Console.Write("\t"+i);                                          //print NEW values
+            Console.WriteLine("New array elements (duplicates omitted):");  
+            for (int i = 0;i < newsize;i++)                                         //print NEW values
+                Console.Write("\t"+newarray[i]);
         }
     }
 }
